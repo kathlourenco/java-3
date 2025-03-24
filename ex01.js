a3e9a24294f4442d92285217253c8bc9ec6e3868
kathlourenco <kathclairesl@gmail.com>
2025-03-24 09:28:06 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,11 @@
+var nome = "kath";
+var sobrenome = "lourenco";
+var ano = 2025;
+var idadeFinal = 18;
+
+var resultadoA = nome + sobrenome;
+var resultadoB = ano - idadeFinal;
+
+console.log(resultadoA);
+console.log(resultadoB);
+
---
+++ main.js
@@ -0,0 +1,29 @@
+var cliente = "Maria";
+var quantidade = 30;
+var preco = 50.5;
+var emEstoque = true;
+
+var numeroA = 1;
+var numeroB = 2;
+var numeroC = 3;
+
+var resultadoSoma = numeroA + numeroB;
+var resultadoSubtracao = numeroB - numeroA;
+var resultadoProduto = numeroA * numeroB;
+var resultadoDivisao = numeroC / numeroB;
+
+console.log(resultadoSubtracao);
+console.log(resultadoSoma);
+console.log(resultadoProduto);
+console.log(resultadoDivisao);
+
+var textoA = "WEB";
+var textoB = "DEV";
+var branco = " ";
+var resultadoA = textoA + textoB;
+var resultadoB = textoB + 1;
+var resultadoC = txtoA + branco + textoB;
+
+console.log(resultadoA);
+console.log(resultadoB);
+console.log(resultadoC);
